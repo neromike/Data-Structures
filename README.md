ab8c27ea333db14a1160b7945cb9217e610ac8e2
Wayne Pereanu <github@pereanu.com>
2018-02-09 17:06:36 -0500

    Counter object implemented

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Data-Structures
+
+Implementing various data structures using basic python and unittest
+	arrays - built-in
+	counter - counter_obj.py
+	
+	TO DO
+		linked lists
+		stacks
+		queues
+		hash-sets
+		hash-maps
+		hash-tables
+		dictionary
+		trees and binary trees
+		heaps
+		graphs
+
---
+++ counter_obj.py
@@ -0,0 +1,41 @@
+class counter_obj(object):
+	"""
+	counter_obj ix a class that supports:
+		tally_count - a class variable for total number of increments
+		increment - an instance method to increment an instance variable (curr_count)
+		to_string - an instance method to return the increment counter as a string
+	"""
+	
+	tally_count = 0
+	
+	def __init__(self):
+		"""Initializes the instance's curr_count to zero."""
+		self.curr_count = 0
+	
+	def increment (self):
+		"""Increments the instance's curr_count by one."""
+		self.curr_count += 1
+		counter_obj.tally_count += 1
+		return
+	
+	def to_string(self):
+		"""Returns the instance's curr_oount as a string"""
+		return str(self.curr_count)
+
+
+import unittest
+class test_counter(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		cls.this_counter = counter_obj()
+		cls.this_counter.increment()
+		cls.this_counter.increment()
+		cls.this_counter2 = counter_obj()
+		cls.this_counter2.increment()
+	def test_increment(self):
+		self.assertEqual(self.this_counter.curr_count, 2)
+	def test_tally_count(self):
+		self.assertEqual(self.this_counter.tally_count, 3)
+	
+if __name__ == '__main__':
+	unittest.main()
