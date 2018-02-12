06333b85da636ec5ece29cd66ad935fe5b4b6d13
Wayne Pereanu <github@pereanu.com>
2018-02-12 09:15:20 -0500

    documentation fix

--- stack_obj.py
+++ stack_obj.py
@@ -1,6 +1,6 @@
 class stack_obj(object):
 	"""
-	linked_list_singly_obj is a class that creates a stack (LIFO) and supports:
+	stack_obj is a class that creates a stack (LIFO) and supports:
 		push - an instance method to add a new node to the head of the stack.
 		pop - an instance method to return the value from the first node of the stack, and remove it.
 		peek - an instance method to return the value from the first node of the stack.
